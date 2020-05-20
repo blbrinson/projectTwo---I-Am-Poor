12a91a784a78b2b859d1cfb1757d74aa157052d1
Brice Brinson <26476112+blbrinson@users.noreply.github.com>
2020-05-20 13:28:39 -0500

    Add files via upload

---
+++ 2 - I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Brice on 4/15/20.
+//  Copyright © 2020 blbrinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
